f8bc5dc6f128e4c82c2add27c4951d4463196d3e
ybxiang05 <brooke.xiang@gmail.com>
2019-11-19 13:27:18 -0800

    init

---
+++ footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package QOD_Starter_Theme
+ */
+
+?>
+
+			</div><!-- #content -->
+
+			<footer id="colophon" class="site-footer" role="contentinfo">
+				<nav id="site-navigation" class="main-navigation" role="navigation">
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
+					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+				</nav><!-- #site-navigation -->
+				<div class="site-info">
+					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+				</div><!-- .site-info -->
+			</footer><!-- #colophon -->
+		</div><!-- #page -->
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
---
+++ js/qod.js
@@ -0,0 +1,11 @@
+(function($) {
+  $('body').append('helloooooooo world');
+  //1. add click event to "Show Me Another" btn
+  //2. get request to grab random post and append to DOM
+  //3. post new quote using the post method
+  //4. have .done(), .fail(), .always() functions
+  //5. post new quote using post method
+  //6. use form to submit quote - .submit() event
+})(jQuery);
+//Immediately Invoked Function Expression, IIFE
+//Invoked = calling/running a function
